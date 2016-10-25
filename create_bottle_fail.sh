50faeb5a8bce7495fa4df6d3ff4bae5ac158f5a2
Hiroaki Nakamura <hnakamur@gmail.com>
2016-10-25 03:58:37 +0000

    (Temporary) Add scripts to create bottle

---
+++ create_bottle_fail.sh
@@ -0,0 +1,11 @@
+curl -i -X POST -d '{
+  "color": "white",
+  "country": "USA",
+  "name": "Number 8",
+  "region": "Napa Valley",
+  "review": "Great and inexpensive",
+  "sweetness": 1,
+  "varietal": "Merlot",
+  "vineyard": "Asti",
+  "vintage": 12
+}' localhost:8080/cellar/accounts/1/bottles
